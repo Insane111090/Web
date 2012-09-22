1f351b6e13a7b60e6bbc66811ace330608b1b2b2
Andrey <volirvag132005@bk.ru>
2012-09-22 16:24:09 +0400

    New math

--- math.py
+++ math.py
@@ -1,9 +1,10 @@
 import sys
 import math
 name=sys.argv[1]
-tochn=sys.argv[2]
+tochn=sys.argv[3]
+print "Enter what you whant to see in format <name> : <accuracy>:"
 tochn = int(tochn)
-if name == 'pi':
+if name =='pi':
 print round(math.pi,tochn)
 if name == 'e':
 print round(math.e,tochn)
