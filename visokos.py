5d39e4cd36e0a5f3e032ba0af9d8ff55f484ecdb
Andrey <volirvag132005@bk.ru>
2012-09-22 16:10:38 +0400

    New visok

--- visokos.py
+++ visokos.py
@@ -1,6 +1,8 @@
+# -*- encoding: utf-8 -*-
 import sys
-year=sys.argv[1]
 while 1:
+try:
+year = sys.argv[1]
 year = int(year)
 if year % 400 ==0:
 print u'Vysokosniy'
@@ -13,4 +15,10 @@ print u'Vysokosniy'
 break
 else:
 print u'ne visokosniy'
+break
+except(ValueError):
+print "Please enter a number"
+break
+except Exception, err:
+print "Unexpected error:", err
 break
