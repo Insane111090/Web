7426288e3e0894e5150931c887a8f6036bf86b78
insane <volirvag132005@bk.ru>
2012-12-22 20:57:29 +0400

    base saved

--- Projects/Web/Course_proj/Vibor_date/date/views.py
+++ Projects/Web/Course_proj/Vibor_date/date/views.py
@@ -1,11 +1,16 @@
 # Create your views here.
 from django.shortcuts import render
 from datetime import datetime
+from date.models import Date
 import os
 
 
 def home(request):
-	context={'ts':datetime.now()}
-	return render(request,'home.html',context)
+	return render(request,'home.html')
 def result(request):
+	name = request.POST.get('user','')
+	date = request.POST.get('entered_date','')
+	newItem = Date(name = name, date = date)
+	newItem.save()
 	return render(request,'result.html')
+
